6a76702b7f95a04c24eec690b8a1ff1145f1cc06
Reginaldo Gutter <reginaldogutter@gmail.com>
2019-08-11 16:40:00 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
+import logo from './assets/logo.svg';
 
 function App() {
-  return <div />;
+  return (
+    <div>
+      <img src={logo} alt="Tindev - Reggie" />
+    </div>
+  );
 }
 
 export default App;
--- src/pages/Login.js
+++
@@ -1,6 +0,0 @@
-// import React from 'react';
-// import logo from '@/assets/logo.svg';
-
-// function Login() {
-//   return <div />;
-// }
